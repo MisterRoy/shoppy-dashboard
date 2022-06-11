467a921541eb8e7611188713c672c5211ee4edb9
M.Roy <regisdam@gmail.com>
2022-06-11 09:45:42 +0000

    🪄 homepage + revenue section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,136 @@
 import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import { earningData, SparklineAreaData, ecomPieChartDat } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProcider";
 
 const Ecommerce = () => {
-  return <div>Ecommerce</div>;
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">$450,000</p>
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-xl"
+            >
+              <button
+                type="button"
+                className="text-2xl rounded-full p-4 hover:drop-shadow-xl"
+                style={{
+                  color: item.iconColor,
+                  backgroundColor: item.iconBg,
+                }}
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates !</p>
+
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+
+                <span>Expense</span>
+              </p>
+
+              <p className="flex items-center gap-2 text-green-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$4000,000</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    25%
+                  </span>
+                </p>
+
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$5000,000</span>
+                </p>
+
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  widht="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+
+              <div className="mt-10">
+                <Button
+                  text="Download Report"
+                  color="white"
+                  bgColor="blue"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Ecommerce;
